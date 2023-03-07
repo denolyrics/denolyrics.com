a1bba889fba932c8a653056fb50745d50643e38e
Nelson Hernández <50471203+nelsondev19@users.noreply.github.com>
2023-03-06 23:17:17 -0600

    Update head.tsx

--- app/pricing/head.tsx
+++ app/pricing/head.tsx
@@ -7,20 +7,22 @@ export default function Head() {
 
   const description =
     "Get text to audio with few clicks through Artificial Intelligence, DenoLyrics is a web application that understands many languages.";
+
+const title = "DenoLyrics Pricing Plans"
   return (
     <>
-      <title>DenoLyrics | Pricing</title>
+      <title>{title}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content={description} />
       <meta name="theme-color" content="#180821" />
-      <meta property="og:title" content="DenoLyrics | Pricing" />
+      <meta property="og:title" content={title} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={DOMAIN} />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content={DOMAIN} />
       <meta name="twitter:creator" content="@denolyrics" />
-      <meta name="twitter:title" content="DenoLyrics | Pricing" />
+      <meta name="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
       <meta property="twitter:image" content="https://www.denolyrics.com/preview.png" />
       <meta
